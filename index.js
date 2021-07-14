7cfea97b3db8e807a09bff92d16607fc1040d5e9
Valerii <valeravivaz@gmail.com>
2021-07-14 13:13:34 +0300

    aaa

---
+++ index.js
@@ -0,0 +1,10 @@
+const chetnoeChisclo = function (value1) {
+  if ((value1 % 2)>0)  {
+    return ('ne chetnoe')
+  } else {
+    return ('chetnoe')
+  }
+}
+
+const x = chetnoeChisclo(1);
+console.log(x);
